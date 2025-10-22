55c99aa6516b13ededcb3ec5629454aae782e30f
Matheus <matheushrm614@gmail.com>
2025-10-21 22:16:15 -0300

    refatora tudo pra gloang

---
+++ feature-gigantesca.go
@@ -0,0 +1,5 @@
+package main
+
+func featureGigantesca() {
+	baita codigo aqui;
+}
---
+++ projeto.go
@@ -0,0 +1,36 @@
+package main
+
+func main() {
+	primeiro commit;
+
+	fixCommit();
+
+	projetoFicandoGrande();
+
+	maisCodigo();
+
+	featureDoLeandro();
+	featureDoSaulo();
+
+	featureGigantesca();
+}
+
+func fixCommit() {
+	fix commit;
+}
+
+func projetoFicandoGrande() {
+	projeto ficando grande;
+}
+
+func maisCodigo() {
+	mais codigo;
+}
+
+func featureDoLeandro() {
+	feature do leandro;
+}
+
+func featureDoSaulo() {
+	feature do saulo;
+}
